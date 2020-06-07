b8084e4f42117f98b417b49cdd00154c4f9a0b10
Shrajan Tiwari <v-shrti@microsoft.com>
2020-06-07 18:12:48 +0530

    app-routing, todo-httpservice

---
+++ Front-End/todo-list-app/src/app/todo-httpService.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+export class TodoHttpService {
+    private baseUrl = "https://localhost:5050";
+  
+    constructor(private http: HttpClient) {}
+
+      getAllTasks() {
+        return this.http.get(this.baseUrl + "/view-All");
+      }
+    
+      getTask(blogId): any {
+        let myResponse = this.http.get(
+          this.baseUrl + "/view" + "/" + blogId 
+        );
+        return myResponse;
+      }
+    
+      deleteTask(blogId): any {
+        let data = {};
+        let myResponse = this.http.post(
+          this.baseUrl + "/" + blogId + "/delete" ,
+          data
+        );
+        return myResponse;
+      }
+    
+        editTask(blogId,blogData): any {
+        let myResponse = this.http.put(this.baseUrl + '/' + blogId + '/edit', blogData)
+        return myResponse;
+      }
+    
+      SendUserData(blogData): any {
+        let myResponse = this.http.post(this.baseUrl + '/registration' , blogData)
+        return myResponse;
+      }
+}
